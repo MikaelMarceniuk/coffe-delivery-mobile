136ba08e25e29d2402cb7250f5f50ef0c8431689
Mikael Marceniuk <mika.marceniuk@gmail.com>
2024-02-15 14:37:35 -0300

    feat(coffeList): Create Section for each Coffe Category

--- src/components/coffeList.tsx
+++ src/components/coffeList.tsx
@@ -1,5 +1,6 @@
-import CoffesMock from '../mocks/coffes.json'
+import { FlatList, View, Text } from 'react-native'
 import styled from 'styled-components/native'
+import CoffesMock from '../mocks/coffes.json'
 import CoffeListFilters from './coffeListFilters'
 
 const Container = styled.View`
@@ -11,6 +12,12 @@ type CoffeCategory = {
   name: string
 }
 
+const SectionTitle = styled.Text`
+  font-family: ${({ theme }) => theme.Typography.Ballo2Bold};
+  font-size: ${({ theme }) => theme.Sizes.Ballo.Xs}px;
+  color: ${({ theme }) => theme.Colors.gray400};
+`
+
 const getCoffesCategories = (): CoffeCategory[] =>
   CoffesMock.map((coffe, i) => ({ id: i, name: coffe.category }))
 
@@ -18,6 +25,16 @@ const CoffeList: React.FC = () => {
   return (
     <Container>
       <CoffeListFilters data={getCoffesCategories()} />
+
+      <FlatList
+        data={CoffesMock}
+        keyExtractor={(_item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <SectionTitle>{item.category}</SectionTitle>
+          </View>
+        )}
+      />
     </Container>
   )
 }
